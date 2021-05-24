d1d0bfa4a5baaec0b88af6028c5f54d89ed829e1
Philipp Böschen <boeschenphilipp@gmail.com>
2021-05-24 18:06:52 +0200

    Initial commit

---
+++ cmd/ncw/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gempir/go-twitch-irc/v2"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	var chatUserDB = make(map[string]struct{})
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		fmt.Printf("%+v\n", chatUserDB)
+		os.Exit(1)
+	}()
+
+	client := twitch.NewAnonymousClient()
+	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
+		fmt.Printf("%s %s - %s\n", message.Time, message.User.Name, message.Message)
+		_, exists := chatUserDB[message.User.ID]
+		if exists {
+			return
+		}
+		chatUserDB[message.User.ID] = struct{}{}
+	})
+	client.Join("Zizaran")
+	err := client.Connect()
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Deichindianer/noita-chat-war
+
+go 1.16
+
+require github.com/gempir/go-twitch-irc/v2 v2.5.0
